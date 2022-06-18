03c8553c1aa78889bec3ed729910b87a022804d2
Alex Chereshnev <aleksejceresnev43@yandex.ru>
2022-06-18 13:23:21 +0300

    Convert DAO to JPA.Specification.

--- src/main/java/ru/yandex/goods/dao/ShopUnitRepository.java
+++ src/main/java/ru/yandex/goods/dao/ShopUnitRepository.java
@@ -1,11 +1,15 @@
 package ru.yandex.goods.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.goods.models.ShopUnitDB;
 
 import java.util.UUID;
 
 @Repository
-public interface ShopUnitRepository extends JpaRepository<ShopUnitDB, UUID>, ShopUnitRepositoryCustom {
+public interface ShopUnitRepository extends JpaRepository<ShopUnitDB, UUID>, JpaSpecificationExecutor<ShopUnitDB> {
+
 }
--- src/main/java/ru/yandex/goods/service/ShopUnitService.java
+++ src/main/java/ru/yandex/goods/service/ShopUnitService.java
@@ -1,11 +1,17 @@
 package ru.yandex.goods.service;
 
+import static org.springframework.data.jpa.domain.Specification.where;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.yandex.goods.dao.ShopUnitRepository;
 import ru.yandex.goods.models.ShopUnit;
 import ru.yandex.goods.models.ShopUnitDB;
+import ru.yandex.goods.models.ShopUnitPrice;
 
+import javax.persistence.criteria.Join;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -20,12 +26,12 @@ public class ShopUnitService {
     }
 
     public ShopUnitDB getShopUnitWithChildren(UUID uuid) {
-        ShopUnitDB unitDB = jpa.getOneWithLastPrice(uuid);
-        Set<ShopUnitDB> children = jpa.getChildrenByParentId(uuid);
+        ShopUnitDB unitDB = jpa.findAll(where(idLike(uuid).and(joinPrices()))).get(0);
+        Set<ShopUnitDB> children = new HashSet<>(jpa.findAll(where(parentIdLike(uuid).and(joinPrices()))));
 
         if (!children.isEmpty()) {
             children.forEach(u ->
-                u.setChildren(getShopUnitWithChildren(u.getId()).getChildren())
+                    u.setChildren(getShopUnitWithChildren(u.getId()).getChildren())
             );
             unitDB.setChildren(children);
         }
@@ -34,5 +40,20 @@ public class ShopUnitService {
 
     public ShopUnitDB save(ShopUnit unit) {
         return jpa.save(unit.convertToShopUnitDB());
+    }
+
+    private Specification<ShopUnitDB> idLike(UUID id) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("id"), id);
+    }
+
+    private Specification<ShopUnitDB> parentIdLike(UUID id) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("parentId"), id);
+    }
+
+    private Specification<ShopUnitDB> joinPrices() {
+        return (root, query, criteriaBuilder) -> {
+            Join<ShopUnitDB, ShopUnitPrice> join = root.join("prices");
+            return criteriaBuilder.equal(join.get("unitId"), root.get("id"));
+        };
     }
 }
