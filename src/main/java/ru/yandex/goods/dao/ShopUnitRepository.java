2709fb972a293324e101f94b8249f5d24ab543df
Alex Chereshnev <aleksejceresnev43@yandex.ru>
2022-06-20 18:13:53 +0300

    Convert DAO to Spring JPARepository

--- src/main/java/ru/yandex/goods/dao/ShopUnitRepository.java
+++ src/main/java/ru/yandex/goods/dao/ShopUnitRepository.java
@@ -3,20 +3,26 @@ package ru.yandex.goods.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.goods.models.ShopUnitDB;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ShopUnitRepository extends JpaRepository<ShopUnitDB, UUID>, JpaSpecificationExecutor<ShopUnitDB> {
+public interface ShopUnitRepository extends JpaRepository<ShopUnitDB, UUID> {
 
     @Query("SELECT unit " +
             "FROM ShopUnitDB unit JOIN FETCH unit.prices price " +
             "WHERE unit.id = ?1 AND price.date BETWEEN ?2 AND ?3")
     ShopUnitDB findByIdAndDate(UUID uuid, LocalDateTime dateStart, LocalDateTime dateEnd);
 
+    Optional<ShopUnitDB> findById(UUID id);
+
+    @Query("SELECT unit " +
+            "FROM ShopUnitDB unit JOIN FETCH unit.prices " +
+            "WHERE unit.parentId = ?1")
+    List<ShopUnitDB> findAllChildrenByParentId(UUID id);
 }
--- src/main/java/ru/yandex/goods/service/ShopUnitService.java
+++ src/main/java/ru/yandex/goods/service/ShopUnitService.java
@@ -1,9 +1,6 @@
 package ru.yandex.goods.service;
 
-import static org.springframework.data.jpa.domain.Specification.where;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.yandex.goods.dao.ShopUnitRepository;
 import ru.yandex.goods.exceptions.NotFoundException;
@@ -11,15 +8,13 @@ import ru.yandex.goods.exceptions.ValidationFailedException;
 import ru.yandex.goods.models.StatisticDates;
 import ru.yandex.goods.models.ShopUnit;
 import ru.yandex.goods.models.ShopUnitDB;
-import ru.yandex.goods.models.ShopUnitPrice;
 
-import javax.persistence.criteria.Join;
 import java.util.*;
 
 @Service
 public class ShopUnitService {
 
-    private ShopUnitRepository jpa;
+    private final ShopUnitRepository jpa;
 
     @Autowired
     public ShopUnitService(ShopUnitRepository jpa) {
@@ -32,7 +27,7 @@ public class ShopUnitService {
 
     private ShopUnitDB combineChildren(UUID uuid) {
         ShopUnitDB unitDB = getShopUnitWithoutChildren(uuid);
-        Set<ShopUnitDB> children = new HashSet<>(jpa.findAll(where(parentIdLike(uuid).and(joinPrices()))));
+        Set<ShopUnitDB> children = new HashSet<>(jpa.findAllChildrenByParentId(uuid));
 
         if (!children.isEmpty()) {
             children.forEach(u ->
@@ -54,6 +49,7 @@ public class ShopUnitService {
             unitDB = Optional
                     .ofNullable(jpa.findByIdAndDate(uuid, dates.getStart(), dates.getEnd()))
                     .orElseThrow(NotFoundException::new);
+
         } else {
             throw new ValidationFailedException();
         }
@@ -63,26 +59,10 @@ public class ShopUnitService {
 
     private ShopUnitDB getShopUnitWithoutChildren(UUID uuid) {
         return jpa
-                .findOne(where(idLike(uuid).and(joinPrices())))
-                .orElseThrow(NotFoundException::new);
+                .findById(uuid).orElseThrow(NotFoundException::new);
     }
 
     public ShopUnitDB save(ShopUnit unit) {
         return jpa.save(unit.convertToShopUnitDB());
-    }
-
-    private Specification<ShopUnitDB> idLike(UUID id) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("id"), id);
-    }
-
-    private Specification<ShopUnitDB> parentIdLike(UUID id) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("parentId"), id);
-    }
-
-    private Specification<ShopUnitDB> joinPrices() {
-        return (root, query, criteriaBuilder) -> {
-            Join<ShopUnitDB, ShopUnitPrice> join = root.join("prices");
-            return criteriaBuilder.equal(join.get("unitId"), root.get("id"));
-        };
     }
 }
